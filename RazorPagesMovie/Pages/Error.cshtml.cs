6d96ec02e2408fe8007dc322dbb187fb11e032ac
liupeng <1482989710@qq.com>
2019-11-04 09:19:19 +0800

    RazorPagesMovie

--- RazorPagesMovie/Pages/Error.cshtml.cs
+++ RazorPagesMovie/Pages/Error.cshtml.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace core_web_lianxi.Pages
+namespace RazorPagesMovie.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
--- RazorPagesMovie/Program.cs
+++ RazorPagesMovie/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace core_web_lianxi
+namespace RazorPagesMovie
 {
     public class Program
     {
